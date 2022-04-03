a9bb5c6f27302215b564bebf2dc4ac162746895f
Benjamin HARTMANN <jpnbenj@gmail.com>
2022-04-03 22:58:11 +0200

    Add game files for tnt process

--- src/main/java/me/test/plugin/hello/Event.java
+++ src/main/java/me/test/plugin/hello/Event.java
@@ -4,15 +4,216 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Event implements Listener {
+    private final ArrayList<TntGame> allTNTFire = new ArrayList<>();
+
+    @EventHandler
+    public void fireDamage(EntityDamageEvent e) {
+        if (e.getEntity() instanceof Player) {
+            if (e.getCause().equals(EntityDamageEvent.DamageCause.FIRE) || e.getCause().equals(EntityDamageEvent.DamageCause.FIRE_TICK)) {
+
+                Player p = (Player) e.getEntity();
+                Location loc = p.getLocation();
+
+                Player killer = getKiller(loc);
+
+                p.sendMessage("Tué par " + killer.getName());
+            }
+        }
+    }
+
+    private Player getKiller(Location location) {
+        AtomicReference<Player> p = null;
+
+        allTNTFire.forEach((tntGame) -> tntGame.getBlocks().forEach((fire) -> {
+            if (fire.getBlockX() == location.getBlockX() && fire.getBlockZ() ==  location.getBlockZ()) {
+                p.set(tntGame.getCreator());
+            }
+        }));
+
+        return p.get();
+    }
+
+    @EventHandler
+    public void onPlace(BlockPlaceEvent event) {
+        Block tnt = event.getBlockPlaced();
+
+        if (tnt.getType() == Material.TNT) {
+            if(tnt.getLocation().add(0, -1, 0).getBlock().getType() == Material.BEDROCK) {
+                TntGame fire = new TntGame(tnt.getLocation(), event.getPlayer());
+                explosion(tnt.getLocation(), fire, 30);
+            } else {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    private void explosion(Location location, TntGame fire, int preTime) {
+        allTNTFire.add(fire);
+
+        new BukkitRunnable()
+        {
+            int time = 10 + preTime;
+            int distance = 1;
+            final int power = 4;
+            boolean Xup = true;
+            boolean Xdown = true;
+            boolean Zup = true;
+            boolean Zdown = true;
+
+            @Override
+            public void run()
+            {
+                if (time > 9 && location.getBlock().getType() == Material.FIRE) {
+                    this.cancel();
+                    clearFire(fire);
+                    allTNTFire.remove(fire);
+                }
+
+                if (time == 10) {
+                    location.getBlock().setType(Material.FIRE);
+                    fire.addLocation(location);
+                }
+
+                if (time < 10) {
+                    if (Xup) {
+                        Block block = location.getWorld().getBlockAt(location.getBlockX() + distance, location.getBlockY(), location.getBlockZ());
+                        Xup = testBlock(block);
+
+                        if (block.getType() == Material.DIRT) {
+                            fire.addLocation(block.getLocation());
+                            breakDirt(block);
+                        }
+
+                        if (Xup) {
+                            fire.addLocation(block.getLocation());
+                        }
+                    }
+
+                    if (Xdown) {
+                        Block block = location.getWorld().getBlockAt(location.getBlockX() - distance, location.getBlockY(), location.getBlockZ());
+                        Xdown = testBlock(block);
+
+                        if (block.getType() == Material.DIRT) {
+                            fire.addLocation(block.getLocation());
+                            breakDirt(block);
+                        }
+
+                        if (Xdown) {
+                            fire.addLocation(block.getLocation());
+                        }
+                    }
+
+                    if (Zup) {
+                        Block block = location.getWorld().getBlockAt(location.getBlockX(), location.getBlockY(), location.getBlockZ() + distance);
+                        Zup = testBlock(block);
+
+                        if (block.getType() == Material.DIRT) {
+                            fire.addLocation(block.getLocation());
+                            breakDirt(block);
+                        }
+
+                        if (Zup) {
+                            fire.addLocation(block.getLocation());
+                        }
+                    }
+
+                    if (Zdown) {
+                        Block block = location.getWorld().getBlockAt(location.getBlockX(), location.getBlockY(), location.getBlockZ() - distance);
+                        Zdown = testBlock(block);
+
+                        if (block.getType() == Material.DIRT) {
+                            fire.addLocation(block.getLocation());
+                            breakDirt(block);
+                        }
+
+                        if (Zdown) {
+                            fire.addLocation(block.getLocation());
+                        }
+                    }
+
+                    if (distance == power) {
+                        clearFire(fire);
+                        allTNTFire.remove(fire);
+                        this.cancel();
+                    }
+
+                    distance++;
+                }
+
+                this.time--;
+            }
+        }.runTaskTimer(Hello.getPlugin(), 0L, 1L);
+    }
+
+    private void clearFire(TntGame fire) {
+        Hello.getPlugin().getServer().getScheduler().scheduleSyncDelayedTask(Hello.getPlugin(), () ->
+            fire.getBlocks().forEach((currentFireBLock) -> {
+
+                if (!isOtherBomb(currentFireBLock)) {
+                    currentFireBLock.getBlock().setType(Material.VOID_AIR);
+                }
+        }), 5L);
+    }
+
+    private boolean isOtherBomb(Location location) {
+        AtomicInteger result = new AtomicInteger();
+
+        allTNTFire.forEach((tntBLocks) -> tntBLocks.getBlocks().forEach((fire) -> {
+            if (fire.getBlockX() == location.getBlockX() && fire.getBlockZ() ==  location.getBlockZ()) {
+                result.getAndIncrement();
+            }
+        }));
+
+        return result.get() >= 1;
+    }
+
+    private void breakDirt(Block block) {
+        Location location = block.getLocation();
+        block.setType(Material.FIRE);
+        block.getWorld().getBlockAt(location.getBlockX(), location.getBlockY() + 1, location.getBlockZ()).setType(Material.VOID_AIR);
+        block.getWorld().getBlockAt(location.getBlockX(), location.getBlockY() + 2, location.getBlockZ()).setType(Material.VOID_AIR);
+    }
+
+    public boolean testBlock(Block block) {
+        if (block.getType() == Material.BEDROCK || block.getType() == Material.DIRT) {
+            return false;
+        }
+
+        if (block.getType() == Material.TNT) {
+            AtomicReference<TntGame> tntGameElem = null;
+
+            allTNTFire.forEach((tntGame) -> {
+                if (tntGame.getLocation().getBlockX() == block.getLocation().getBlockX()
+                    && tntGame.getLocation().getBlockZ() == block.getLocation().getBlockZ()) {
+                    tntGameElem.set(tntGame);
+                }
+            });
+
+            explosion(block.getLocation(), tntGameElem.get(), 0);
+            return false;
+        }
+
+        block.setType(Material.FIRE);
+
+        return true;
+    }
 
     @EventHandler
     public void onPlayerMove(PlayerInteractEvent event) {
@@ -29,8 +230,29 @@ public class Event implements Listener {
             Location blockLoc = event.getClickedBlock().getLocation();
             PersistentDataContainer container = player.getWorld().getPersistentDataContainer();
 
+            int b1X = container.has(new NamespacedKey(Hello.getPlugin(), "b1X"), PersistentDataType.INTEGER) ?
+                    container.get(new NamespacedKey(Hello.getPlugin(), "b1X"), PersistentDataType.INTEGER) : 0;
+            int b1Y = container.has(new NamespacedKey(Hello.getPlugin(), "b1Y"), PersistentDataType.INTEGER) ?
+                    container.get(new NamespacedKey(Hello.getPlugin(), "b1Y"), PersistentDataType.INTEGER): 0 ;
+            int b1Z = container.has(new NamespacedKey(Hello.getPlugin(), "b1Z"), PersistentDataType.INTEGER) ?
+                    container.get(new NamespacedKey(Hello.getPlugin(), "b1Z"), PersistentDataType.INTEGER) : 0;
+            int b2X = container.has(new NamespacedKey(Hello.getPlugin(), "b2X"), PersistentDataType.INTEGER) ?
+                    container.get(new NamespacedKey(Hello.getPlugin(), "b2X"), PersistentDataType.INTEGER) : 0;
+            int b2Y = container.has(new NamespacedKey(Hello.getPlugin(), "b2Y"), PersistentDataType.INTEGER) ?
+                    container.get(new NamespacedKey(Hello.getPlugin(), "b2Y"), PersistentDataType.INTEGER) : 0;
+            int b2Z = container.has(new NamespacedKey(Hello.getPlugin(), "b2Z"), PersistentDataType.INTEGER) ?
+                    container.get(new NamespacedKey(Hello.getPlugin(), "b2Z"), PersistentDataType.INTEGER) : 0;
+
             if (action == Action.RIGHT_CLICK_BLOCK) {
-                player.sendMessage(ChatColor.GREEN + "You've right clicked while holding the Magic Pickaxe.");
+                if (blockLoc.getBlockY() != b2Y) {
+                    player.sendMessage(ChatColor.RED + "Pas le même axe X");
+                }
+
+                if ((Math.abs(blockLoc.getBlockX() - b2X)%2 != 0) || (Math.abs(blockLoc.getBlockZ() - b2Z)%2 != 0)) {
+                    player.sendMessage(ChatColor.RED + "Pas les bonnes dimensions");
+                    player.sendMessage(ChatColor.RED + "Pas les bonnes dimensions X:"
+                            + Math.abs(blockLoc.getBlockX() - b2X)%2 + " Z: " + Math.abs(blockLoc.getBlockZ() - b2Z)%2);
+                }
 
                 container.set(
                         new NamespacedKey(Hello.getPlugin(), "b1X"),
@@ -48,7 +270,15 @@ public class Event implements Listener {
                         blockLoc.getBlockZ()
                 );
             } else if (action == Action.LEFT_CLICK_BLOCK) {
-                player.sendMessage(ChatColor.RED + "You've right clicked while holding the Magic Pickaxe.");
+                if (b1Y != blockLoc.getBlockY()) {
+                    player.sendMessage(ChatColor.RED + "Pas le même axe X");
+                }
+
+                if (Math.abs(blockLoc.getBlockX() - b1X)%2 !=0 || Math.abs(blockLoc.getBlockZ() - b1Z)%2 != 0) {
+                    player.sendMessage(ChatColor.RED + "Pas les bonnes dimensions X:"
+                            + Math.abs(blockLoc.getBlockX() - b1X)%2 + " Z: " + Math.abs(blockLoc.getBlockZ() - b1Z)%2);
+                }
+
 
                 container.set(
                         new NamespacedKey(Hello.getPlugin(), "b2X"),
@@ -66,22 +296,6 @@ public class Event implements Listener {
                         blockLoc.getBlockZ()
                 );
             }
-
-            int b1X = container.has(new NamespacedKey(Hello.getPlugin(), "b1X"), PersistentDataType.INTEGER) ?
-                    container.get(new NamespacedKey(Hello.getPlugin(), "b1X"), PersistentDataType.INTEGER) : 0;
-            int b1Y = container.has(new NamespacedKey(Hello.getPlugin(), "b1Y"), PersistentDataType.INTEGER) ?
-                    container.get(new NamespacedKey(Hello.getPlugin(), "b1Y"), PersistentDataType.INTEGER): 0 ;
-            int b1Z = container.has(new NamespacedKey(Hello.getPlugin(), "b1Z"), PersistentDataType.INTEGER) ?
-                    container.get(new NamespacedKey(Hello.getPlugin(), "b1Z"), PersistentDataType.INTEGER) : 0;
-            int b2X = container.has(new NamespacedKey(Hello.getPlugin(), "b2X"), PersistentDataType.INTEGER) ?
-                    container.get(new NamespacedKey(Hello.getPlugin(), "b2X"), PersistentDataType.INTEGER) : 0;
-            int b2Y = container.has(new NamespacedKey(Hello.getPlugin(), "b2Y"), PersistentDataType.INTEGER) ?
-                    container.get(new NamespacedKey(Hello.getPlugin(), "b2Y"), PersistentDataType.INTEGER) : 0;
-            int b2Z = container.has(new NamespacedKey(Hello.getPlugin(), "b2Z"), PersistentDataType.INTEGER) ?
-                    container.get(new NamespacedKey(Hello.getPlugin(), "b2Z"), PersistentDataType.INTEGER) : 0;
-
-            player.sendMessage("b1X : " + b1X + " | b1Y : " + b1Y + " | b1Z : " + b1Z);
-            player.sendMessage("b2X : " + b2X + " | b2Y : " + b2Y + " | b2Z : " + b2Z);
         }
     }
 }
--- src/main/java/me/test/plugin/hello/SetSpawnCommand.java
+++ src/main/java/me/test/plugin/hello/SetSpawnCommand.java
@@ -2,10 +2,8 @@ package me.test.plugin.hello;
 
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
-import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,6 +13,7 @@ import org.bukkit.persistence.PersistentDataType;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Random;
 
 public class SetSpawnCommand implements CommandExecutor {
 
@@ -41,46 +40,169 @@ public class SetSpawnCommand implements CommandExecutor {
             int b2Z = container.has(new NamespacedKey(Hello.getPlugin(), "b2Z"), PersistentDataType.INTEGER) ?
                     container.get(new NamespacedKey(Hello.getPlugin(), "b2Z"), PersistentDataType.INTEGER) : 0;
 
-
             if (b1Y != b2Y) {
                 return false;
             }
 
-            player.sendMessage("b1X : " + b1X + " | b1Y : " + b1Y + " | b1Z : " + b1Z);
-            player.sendMessage("b2X : " + b2X + " | b2Y : " + b2Y + " | b2Z : " + b2Z);
-
             if (b1X < b2X) {
-                for (int i = b1X; i < b2X; i++) {
+                for (int i = b1X; i <= b2X; i++) {
                     if (b1Z < b2Z) {
-                        for (int j = b1Z + 1; j < b2Z; j++) {
-                            Block block = player.getWorld().getBlockAt(i, b1Y, j);
-                            block.setType(Material.DIRT);
+                        for (int j = b1Z; j <= b2Z; j++) {
+                            player.getWorld().getBlockAt(i, b1Y, j).setType(Material.VOID_AIR);
+                            player.getWorld().getBlockAt(i, b1Y+1, j).setType(Material.VOID_AIR);
+                            player.getWorld().getBlockAt(i, b1Y+2, j).setType(Material.VOID_AIR);
+                            player.getWorld().getBlockAt(i, b1Y+3, j).setType(Material.VOID_AIR);
                         }
                     } else {
-                        for (int j = b2Z; j < b1Z; j++) {
-                            Block block = player.getWorld().getBlockAt(i, b1Y, j);
-                            block.setType(Material.DIRT);
+                        for (int j = b2Z; j <= b1Z; j++) {
+                            player.getWorld().getBlockAt(i, b1Y, j).setType(Material.VOID_AIR);
+                            player.getWorld().getBlockAt(i, b1Y+1, j).setType(Material.VOID_AIR);
+                            player.getWorld().getBlockAt(i, b1Y+2, j).setType(Material.VOID_AIR);
+                            player.getWorld().getBlockAt(i, b1Y+3, j).setType(Material.VOID_AIR);
                         }
                     }
                 }
             } else {
-                for (int i = b2X; i < b1X; i++) {
+                for (int i = b2X; i <= b1X; i++) {
                     if (b1Z < b2Z) {
-                        for (int j = b1Z; j < b2Z; j++) {
-                            Block block = player.getWorld().getBlockAt(i, b1Y, j);
-                            block.setType(Material.DIRT);
+                        for (int j = b1Z; j <= b2Z; j++) {
+                            player.getWorld().getBlockAt(i, b1Y, j).setType(Material.VOID_AIR);
+                            player.getWorld().getBlockAt(i, b1Y+1, j).setType(Material.VOID_AIR);
+                            player.getWorld().getBlockAt(i, b1Y+2, j).setType(Material.VOID_AIR);
+                            player.getWorld().getBlockAt(i, b1Y+3, j).setType(Material.VOID_AIR);
                         }
                     } else {
-                        for (int j = b2Z; j < b1Z; j++) {
-                            Block block = player.getWorld().getBlockAt(i, b1Y, j);
-                            block.setType(Material.DIRT);
+                        for (int j = b2Z; j <= b1Z; j++) {
+                            player.getWorld().getBlockAt(i, b1Y, j).setType(Material.VOID_AIR);
+                            player.getWorld().getBlockAt(i, b1Y+1, j).setType(Material.VOID_AIR);
+                            player.getWorld().getBlockAt(i, b1Y+2, j).setType(Material.VOID_AIR);
+                            player.getWorld().getBlockAt(i, b1Y+3, j).setType(Material.VOID_AIR);
+                        }
+                    }
+                }
+            }
+
+            if (b1X < b2X) {
+                for (int i = b1X; i <= b2X; i++) {
+                    if (b1Z < b2Z) {
+                        for (int j = b1Z; j <= b2Z; j++) {
+                            if (i == b1X || i == b2X || j == b1Z || j == b2Z ||
+                                            ((Math.abs(j - b1Z)%2) == 0) && ((Math.abs(i - b1X)%2) == 0)) {
+                                player.getWorld().getBlockAt(i, b1Y+1, j).setType(Material.BEDROCK);
+                                player.getWorld().getBlockAt(i, b1Y+2, j).setType(Material.BEDROCK);
+                                player.getWorld().getBlockAt(i, b1Y+3, j).setType(Material.BEDROCK);
+                            } else {
+                                boolean condition1 = (Math.abs(i - b1X) == 1 || Math.abs(i - b1X) == 2);
+                                boolean condition2 = (Math.abs(j - b1Z) == 1 || Math.abs(j - b1Z) == 2);
+                                boolean condition3 = (i == b2X - 1 || i == b2X - 2);
+                                boolean condition4 = (j == b2Z - 1 || j == b2Z - 2);
+
+                                if (!(condition1 && condition2) && !(condition3 && condition4)
+                                        && !(condition1 && condition4) && !(condition3 && condition2)) {
+                                    Random r = new Random();
+
+                                    if (r.nextInt(100) > 50) {
+                                        player.getWorld().getBlockAt(i, b1Y+1, j).setType(Material.DIRT);
+                                        player.getWorld().getBlockAt(i, b1Y+2, j).setType(Material.DIRT);
+                                        player.getWorld().getBlockAt(i, b1Y+3, j).setType(Material.DIRT);
+                                    }
+                                }
+                            }
+
+                            player.getWorld().getBlockAt(i, b1Y, j).setType(Material.BEDROCK);
+                        }
+                    } else {
+                        for (int j = b2Z; j <= b1Z; j++) {
+                            if (i == b1X || i == b2X || j == b1Z || j == b2Z ||
+                                    ((Math.abs(j - b2Z)%2) == 0) && ((Math.abs(i - b1X)%2) == 0)) {
+                                player.getWorld().getBlockAt(i, b1Y+1, j).setType(Material.BEDROCK);
+                                player.getWorld().getBlockAt(i, b1Y+2, j).setType(Material.BEDROCK);
+                                player.getWorld().getBlockAt(i, b1Y+3, j).setType(Material.BEDROCK);
+                            } else {
+                                boolean condition1 = (Math.abs(i - b1X) == 1 || Math.abs(i - b1X) == 2);
+                                boolean condition2 = (Math.abs(j - b2Z) == 1 || Math.abs(j - b2Z) == 2);
+                                boolean condition3 = (i == b2X - 1 || i == b2X - 2);
+                                boolean condition4 = (j == b1Z - 1 || j == b1Z - 2);
+
+                                if (!(condition1 && condition2) && !(condition3 && condition4)
+                                        && !(condition1 && condition4) && !(condition3 && condition2)) {
+                                    Random r = new Random();
+
+                                    if (r.nextInt(100) > 50) {
+                                        player.getWorld().getBlockAt(i, b1Y+1, j).setType(Material.DIRT);
+                                        player.getWorld().getBlockAt(i, b1Y+2, j).setType(Material.DIRT);
+                                        player.getWorld().getBlockAt(i, b1Y+3, j).setType(Material.DIRT);
+                                    }
+                                }
+                            }
+
+                            player.getWorld().getBlockAt(i, b1Y, j).setType(Material.BEDROCK);
+                        }
+                    }
+                }
+            } else {
+                for (int i = b2X; i <= b1X; i++) {
+                    if (b1Z < b2Z) {
+                        for (int j = b1Z; j <= b2Z; j++) {
+                            if (i == b1X || i == b2X || j == b1Z || j == b2Z ||
+                                    ((Math.abs(j - b1Z)%2) == 0) && ((Math.abs(i - b2X)%2) == 0)) {
+                                player.getWorld().getBlockAt(i, b1Y+1, j).setType(Material.BEDROCK);
+                                player.getWorld().getBlockAt(i, b1Y+2, j).setType(Material.BEDROCK);
+                                player.getWorld().getBlockAt(i, b1Y+3, j).setType(Material.BEDROCK);
+                            } else {
+                                boolean condition1 = (Math.abs(i - b2X) == 1 || Math.abs(i - b2X) == 2);
+                                boolean condition2 = (Math.abs(j - b1Z) == 1 || Math.abs(j - b1Z) == 2);
+                                boolean condition3 = (i == b1X - 1 || i == b1X - 2);
+                                boolean condition4 = (j == b2Z - 1 || j == b2Z - 2);
+
+                                if (!(condition1 && condition2) && !(condition3 && condition4)
+                                        && !(condition1 && condition4) && !(condition3 && condition2)) {
+                                    Random r = new Random();
+
+                                    if (r.nextInt(100) > 50) {
+                                        player.getWorld().getBlockAt(i, b1Y+1, j).setType(Material.DIRT);
+                                        player.getWorld().getBlockAt(i, b1Y+2, j).setType(Material.DIRT);
+                                        player.getWorld().getBlockAt(i, b1Y+3, j).setType(Material.DIRT);
+                                    }
+                                }
+                            }
+
+
+                            player.getWorld().getBlockAt(i, b1Y, j).setType(Material.BEDROCK);
+                        }
+                    } else {
+                        for (int j = b2Z; j <= b1Z; j++) {
+                            if (i == b1X || i == b2X || j == b1Z || j == b2Z ||
+                                    ((Math.abs(j - b2Z)%2) == 0) && ((Math.abs(i - b2X)%2) == 0)) {
+                                player.getWorld().getBlockAt(i, b1Y+1, j).setType(Material.BEDROCK);
+                                player.getWorld().getBlockAt(i, b1Y+2, j).setType(Material.BEDROCK);
+                                player.getWorld().getBlockAt(i, b1Y+3, j).setType(Material.BEDROCK);
+                            } else {
+                                boolean condition1 = (Math.abs(i - b2X) == 1 || Math.abs(i - b2X) == 2);
+                                boolean condition2 = (Math.abs(j - b2Z) == 1 || Math.abs(j - b2Z) == 2);
+                                boolean condition3 = (i == b1X - 1 || i == b1X - 2);
+                                boolean condition4 = (j == b1Z - 1 || j == b1Z - 2);
+
+                                if (!(condition1 && condition2) && !(condition3 && condition4)
+                                        && !(condition1 && condition4) && !(condition3 && condition2)) {
+                                    Random r = new Random();
+
+                                    if (r.nextInt(100) > 50) {
+                                        player.getWorld().getBlockAt(i, b1Y+1, j).setType(Material.DIRT);
+                                        player.getWorld().getBlockAt(i, b1Y+2, j).setType(Material.DIRT);
+                                        player.getWorld().getBlockAt(i, b1Y+3, j).setType(Material.DIRT);
+                                    }
+                                }
+                            }
+
+                            player.getWorld().getBlockAt(i, b1Y, j).setType(Material.BEDROCK);
                         }
                     }
                 }
             }
 
             for (Player p : Hello.getPlugin().getServer().getOnlinePlayers()) {
-                p.teleport(new Location(player.getWorld(), mid(b1X, b2X), b1Y + 3, mid(b1Z, b2Z)));
+//                p.teleport(new Location(player.getWorld(), mid(b1X, b2X), b1Y + 3, mid(b1Z, b2Z)));
             }
 
             Hello.getPlugin().getServer().getScheduler().runTaskTimer(Hello.getPlugin(), new Runnable()
